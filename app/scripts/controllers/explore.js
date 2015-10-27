1c53e97e2fca43bc96c217336b340fd951c3500e
Don McCurdy <donmccurdy@google.com>
2015-10-26 21:11:34 -0700

    wire to tiny apiara API

--- app/scripts/controllers/explore.js
+++ app/scripts/controllers/explore.js
@@ -8,19 +8,14 @@
  * Controller of the sandboxFluxApp
  */
 angular.module('sandboxFluxApp')
-	.controller('ExploreCtrl', function ($scope) {
+	.controller('ExploreCtrl', function ($scope, $http, RESOURCES) {
 		/* Chart.js demo
 		************************************/
-
-		$scope.labels = [
-			'January', 'February', 'March', 'April', 'May', 'June', 'July'
-		];
-		$scope.series = ['Series A', 'Series B'];
-		$scope.data = [
-			[65, 59, 80, 81, 56, 55, 40],
-			[28, 48, 40, 19, 86, 27, 90]
-		];
-		$scope.onClick = function (points, evt) {
-			console.log(points, evt);
-		};
+		$http.get(RESOURCES.API + '/questions')
+			.then(function (response) {
+				var question = _.first(response.data);
+				$scope.labels = _.pluck(question.choices, 'choice');
+				$scope.data = [ _.pluck(question.choices, 'votes') ];
+				$scope.series = [ question.question ];
+			});
 	});
--- test/karma.conf.js
+++ test/karma.conf.js
@@ -35,6 +35,7 @@ module.exports = function(config) {
 			'bower_components/flux/dist/Flux.js',
 			'bower_components/Chart.js/Chart.js',
 			'bower_components/angular-chart.js/dist/angular-chart.js',
+			'bower_components/lodash/lodash.js',
 			'bower_components/angular-mocks/angular-mocks.js',
 			// endbower
 			"app/scripts/**/*.js",
