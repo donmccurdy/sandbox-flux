5be24e7a13a5e5cd76e6a23ed13c452c2728c070
Don McCurdy <donmccurdy@google.com>
2015-10-27 23:04:35 -0700

    add navigation toggle test

--- test/spec/controllers/navigation.js
+++ test/spec/controllers/navigation.js
@@ -5,19 +5,32 @@ describe('Controller: NavigationCtrl', function () {
   // load the controller's module
   beforeEach(module('sandboxFluxApp'));
 
+  // override _.debounce
+  beforeEach(function () {
+    _.debounce = function (callback) { return callback; };
+  });
+
   var NavigationCtrl,
+    isOpen,
     scope;
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
+    isOpen = false;
     NavigationCtrl = $controller('NavigationCtrl', {
-      $scope: scope
-      // place here mocked dependencies
+      $scope: scope,
+      $mdSidenav: function () {
+        return {
+          toggle: function () { isOpen = !isOpen; }
+        };
+      }
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(NavigationCtrl.awesomeThings.length).toBe(3);
+  it('should toggle navigation open and closed', function () {
+    expect(isOpen).toBe(false);
+    scope.toggleNavigation();
+    expect(isOpen).toBe(true);
   });
 });
