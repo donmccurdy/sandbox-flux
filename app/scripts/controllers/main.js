673c0cdf41fe9b5fc8e46ecea6d54a03d9505cf1
Don McCurdy <don.r.mccurdy@gmail.com>
2015-11-01 18:21:01 -0500

    Move controls into sidebar

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,21 +8,6 @@
  * Controller of the sandboxFluxApp
  */
 angular.module('sandboxFluxApp')
-	.controller('MainCtrl', function ($scope, TopicStore, CountryStore) {
-		$scope.topics = TopicStore.topics;
-		$scope.countries = CountryStore.countries;
+	.controller('MainCtrl', function () {
 
-		this.topic = '';
-		this.country = '';
-
-		var tokens = [
-			TopicStore.addListener(function () { $scope.topics = TopicStore.topics; }),
-			CountryStore.addListener(function () { $scope.countries = CountryStore.countries; })
-		];
-
-		$scope.$on('$destroy', function () {
-			_.forEach(tokens, function (token) {
-				token.remove();
-			});
-		});
 	});
---
+++ app/scripts/controllers/sidebar.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sandboxFluxApp.controller:SidebarCtrl
+ * @description
+ * # SidebarCtrl
+ * Controller of the sandboxFluxApp
+ */
+angular.module('sandboxFluxApp')
+	.controller('SidebarCtrl', function ($scope, TopicStore, CountryStore) {
+		$scope.topics = TopicStore.topics;
+		$scope.countries = CountryStore.countries;
+
+		this.topic = '';
+		this.country = '';
+
+		var tokens = [
+			TopicStore.addListener(function () { $scope.topics = TopicStore.topics; }),
+			CountryStore.addListener(function () { $scope.countries = CountryStore.countries; })
+		];
+
+		$scope.$on('$destroy', function () {
+			_.forEach(tokens, function (token) {
+				token.remove();
+			});
+		});
+	});
