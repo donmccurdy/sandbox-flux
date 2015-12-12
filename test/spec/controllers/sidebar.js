5b4a5b35f80cb6ec37d143fbba3beb792063e503
Don McCurdy <don.r.mccurdy@gmail.com>
2015-12-11 23:58:53 -0500

    Mock dependencies of SidebarCtrl

--- test/spec/controllers/sidebar.js
+++ test/spec/controllers/sidebar.js
@@ -2,22 +2,25 @@
 
 describe('Controller: SidebarCtrl', function () {
 
-  // load the controller's module
-  beforeEach(module('wdiApp'));
+	// load the controller's module
+	beforeEach(module('wdiApp'));
 
-  var SidebarCtrl,
-    scope;
+	var sidebarCtrl,
+		scope;
 
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    SidebarCtrl = $controller('SidebarCtrl', {
-      $scope: scope
-      // place here mocked dependencies
-    });
-  }));
+	// Initialize the controller and a mock scope
+	beforeEach(inject(function ($controller, $rootScope) {
+		scope = $rootScope.$new();
+		sidebarCtrl = $controller('SidebarCtrl', {
+			$scope: scope,
+			TopicStore: jasmine.createSpyObj('TopicStore', ['all', 'addListener']),
+			CountryStore: jasmine.createSpyObj('CountryStore', ['all', 'addListener']),
+			IndicatorStore: jasmine.createSpyObj('IndicatorStore', ['getByTopic']),
+			SelectionStore: jasmine.createSpyObj('SelectionStore', ['countries', 'addListener']),
+		});
+	}));
 
-  it('should just exist', function () {
-    expect(true).toBe(true);
-  });
+	it('should just exist', function () {
+		expect(!!sidebarCtrl).toBe(true);
+	});
 });
