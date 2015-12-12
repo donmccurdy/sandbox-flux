3cfd5ebc49953ff0d3f4907fd73b40a507e52531
Don McCurdy <don.r.mccurdy@gmail.com>
2015-12-12 00:08:25 -0500

    Mock dependencies of IndicatorChartCtrl. All tests now pass.

--- test/spec/controllers/indicatorchart.js
+++ test/spec/controllers/indicatorchart.js
@@ -2,22 +2,24 @@
 
 describe('Controller: IndicatorChartCtrl', function () {
 
-  // load the controller's module
-  beforeEach(module('wdiApp'));
+	// load the controller's module
+	beforeEach(module('wdiApp'));
 
-  var indicatorChartCtrl,
-    scope;
+	var indicatorChartCtrl,
+		scope;
 
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    indicatorChartCtrl = $controller('IndicatorChartCtrl', {
-      $scope: scope,
-      $attrs: {}
-    });
-  }));
+	// Initialize the controller and a mock scope
+	beforeEach(inject(function ($controller, $rootScope) {
+		scope = $rootScope.$new();
+		indicatorChartCtrl = $controller('IndicatorChartCtrl', {
+			$scope: scope,
+			$attrs: {},
+			CountryStore: jasmine.createSpyObj('CountryStore', ['getByIso2Code']),
+			SelectionStore: jasmine.createSpyObj('SelectionStore', ['countries', 'addListener']),
+		});
+	}));
 
-  it('should just exist', function () {
-    expect(indicatorChartCtrl).toBeTruthy();
-  });
+	it('should just exist', function () {
+		expect(indicatorChartCtrl).toBeTruthy();
+	});
 });
