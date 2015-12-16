2b3845f7abebe9166ce0aedd2289784f9a3a8ac9
Don McCurdy <don.r.mccurdy@gmail.com>
2015-12-15 23:19:22 -0500

    More tests for navigation

--- test/spec/controllers/navigation.js
+++ test/spec/controllers/navigation.js
@@ -2,36 +2,45 @@
 
 describe('Controller: NavigationCtrl', function () {
 
-  // load the controller's module
-  beforeEach(module('wdiApp'));
+	// load the controller's module
+	beforeEach(module('wdiApp'));
 
-  // override _.debounce
-  beforeEach(function () {
-    _.debounce = function (callback) { return callback; };
-  });
+	// override _.debounce
+	beforeEach(function () {
+		_.debounce = function (callback) { return callback; };
+	});
 
-  var NavigationCtrl,
-    isOpen,
-    scope;
+	var navigationCtrl,
+		isOpen,
+		toggleSpy = jasmine.createSpy('toggle'),
+		path,
+		$scope,
+		$location;
 
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    isOpen = false;
-    NavigationCtrl = $controller('NavigationCtrl', {
-      $scope: scope,
-      $timeout: function (f) { f(); },
-      $mdSidenav: function () {
-        return {
-          toggle: function () { isOpen = !isOpen; }
-        };
-      }
-    });
-  }));
+	// Initialize the controller and a mock scope
+	beforeEach(inject(function ($controller, $rootScope) {
+		$scope = $rootScope.$new();
+		$location = {path: function () { return path; }};
+		isOpen = false;
+		navigationCtrl = $controller('NavigationCtrl', {
+			$scope: $scope,
+			$timeout: function (f) { f(); },
+			$location: $location,
+			$mdSidenav: function () { return {toggle: toggleSpy}; }
+		});
+	}));
 
-  it('should toggle navigation open and closed', function () {
-    expect(isOpen).toBe(false);
-    scope.toggleNavigation();
-    expect(isOpen).toBe(true);
-  });
+	it('should toggle navigation open and closed', function () {
+		expect(toggleSpy).not.toHaveBeenCalled();
+		$scope.toggleNavigation();
+		expect(toggleSpy).toHaveBeenCalled();
+	});
+
+	it('should mark current path as active', function () {
+		path = '/mypath';
+		expect($scope.isActive('/notmypath')).toBe(false);
+		expect($scope.isActive('/mypath')).toBe(true);
+		path = '/otherpath';
+		expect($scope.isActive('/mypath')).toBe(false);
+	});
 });
